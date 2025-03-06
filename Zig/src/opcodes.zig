dcd918247501cc142b0e9bfc7602f3dfcdd3f808
Adam Parker <30392071+grab-a-byte@users.noreply.github.com>
2025-03-06 20:11:32 +0000

    add more opcodes

--- Zig/src/opcodes.zig
+++ Zig/src/opcodes.zig
@@ -16,7 +16,20 @@ fn getr16MemName(val: u2) []const u8 {
         0 => "bc",
         1 => "de",
         2 => "hl+",
-        3 => "hl-"
+        3 => "hl-",
+    };
+}
+
+fn getR8Name(val: u3) []const u8 {
+    return switch (val) {
+        0 => "b",
+        1 => "c",
+        2 => "d",
+        3 => "e",
+        4 => "h",
+        5 => "l",
+        6 => "[hl]",
+        7 => "a",
     };
 }
 
@@ -81,6 +94,55 @@ pub fn disassembleInstruction(allocator: std.mem.Allocator, bytes: []const u8) !
             const ins = try std.fmt.allocPrint(allocator, "add hl, {s}", .{paramName});
             return .{ .str = ins, .length = 1 };
         },
+
+        // inc r8
+        0x04, 0x14, 0x24, 0x34, 0x0C, 0x1C, 0x2C, 0x3C => {
+            const param: u3 = @truncate(instruction >> 3);
+            const paramName = getR8Name(param);
+            const ins = try std.fmt.allocPrint(allocator, "inc {s}", paramName);
+            return .{ .str = ins, .length = 1 };
+        },
+
+        //dec r8
+        0x05, 0x15, 0x25, 0x35, 0x0D, 0x1D, 0x2D, 0x3D => {
+            const param: u3 = @truncate(instruction >> 3);
+            const paramName = getR8Name(param);
+            const ins = try std.fmt.allocPrint(allocator, "dec {s}", paramName);
+            return .{ .str = ins, .length = 1 };
+        },
+
+        //ld r8, imm8
+        0x06, 0x0E, 0x16, 0x1E, 0x26, 0x2E, 0x36, 0x3E => {
+            const param: u3 = @truncate(instruction >> 3);
+            const paramName = getR8Name(param);
+            const value = bytes[1];
+            const ins = try std.fmt.allocPrint(allocator, "ld {s}, {d}", .{ paramName, value });
+            return .{ .str = ins, .length = 2 };
+        },
+
+        //rlca
+        0x07 => return .{ .str = "rlca", .length = 1 },
+
+        //rrca
+        0x0F => return .{ .str = "rrca", .length = 1 },
+
+        //rla
+        0x17 => return .{ .str = "rla", .length = 1 },
+
+        // rra
+        0x1F => return .{ .str = "rra", .length = 1 },
+
+        // daa
+        0x27 => return .{ .str = "daa", .length = 1 },
+
+        //cpl
+        0x2F => return .{ .str = "cpl", .length = 1 },
+
+        //scf
+        0x37 => return .{ .str = "scf", .length = 1 },
+
+        //ccf
+        0x18 => return .{ .str = "ccf", .length = 1 },
 
         else => unreachable,
     };
